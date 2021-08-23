016ba51758a30310a5453f9aa26601e07ea4c9a8
robert <matthewrueter51@gmail.com>
2021-08-23 15:35:26 +0200

    grid creation and clearing buttons

--- esketch.js
+++ esketch.js
@@ -1,14 +1,41 @@
+let size;
 const gridContainer =document.querySelector("#gridContainer");
 let gridItem=document.createElement('div');
 
-//functions to create grids.
+//buttons below
+const clear= document.querySelector("#clear");
+const grid16=document.querySelector("#small");
+const grid32=document.querySelector("#medium");
+const grid64=document.querySelector("#large");
 
+//button click events below
+clear.addEventListener('click', ()=>{
+    clearGrid();
+})
+
+grid16.addEventListener('click', ()=>{
+    createGrid(16);
+});
+grid32.addEventListener("click", ()=>{
+    createGrid(32);
+});
+grid64.addEventListener("click", ()=>{
+    createGrid(64);
+});
+
+//these two functions create the grid and clear the grid and are called with click events from above.
 function createGrid(x){
+    clearGrid(parent);
     let size=x**2
    for(i=0;i<size;i++){
         gridItem=document.createElement('div');
         gridContainer.appendChild(gridItem);
    }  
-}
+};
 
-createGrid(64);
+function clearGrid(parent){
+parent=gridContainer;
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);   
+        }
+};
