b70e0d85a7157e6c9451cf50048df2c6afc50bb7
robert <matthewrueter51@gmail.com>
2021-08-30 13:49:58 +0200

    color menu buttons now work

--- esketch.js
+++ esketch.js
@@ -5,6 +5,10 @@ const gridContainer=document.querySelector("#gridContainer");
 //buttons below
 const black=document.querySelector('#black');
 const color=document.querySelector('#color');
+    const red=document.querySelector('#red');
+    const blue=document.querySelector('#blue');
+    const green=document.querySelector('#green');
+
 const eraser=document.querySelector("#eraser");
 const clear= document.querySelector("#clear");
 const grid16=document.querySelector("#small");
@@ -23,6 +27,8 @@ black.addEventListener('click', ()=>{
         });
 color.addEventListener('click', ()=>{
     paintColor();
+    let colorMenuActive=document.querySelector('.colorMenuActive');
+    colorMenuActive.style.display="flex";
 });
         color.addEventListener('mousedown', ()=>{
             color.style.backgroundColor="white";
@@ -30,6 +36,39 @@ color.addEventListener('click', ()=>{
             color.addEventListener('mouseup', ()=>{
             color.style.backgroundColor="#b00505";
         });
+                            blue.addEventListener('click', ()=>{
+                                paintBlue();
+                                let colorMenuActive=document.querySelector('.colorMenuActive');
+                                colorMenuActive.style.display="none";
+                            });
+                                blue.addEventListener('mousedown', ()=>{
+                                    blue.style.backgroundColor="gray";
+                                });
+                                blue.addEventListener('mouseup', ()=>{
+                                    blue.style.backgroundColor="white";
+                                });
+                                            red.addEventListener('click', ()=>{
+                                                paintRed();
+                                                let colorMenuActive=document.querySelector('.colorMenuActive');
+                                                colorMenuActive.style.display="none";
+                                            });
+                                                red.addEventListener('mousedown', ()=>{
+                                                    red.style.backgroundColor="gray";
+                                                });
+                                                red.addEventListener('mouseup', ()=>{
+                                                    red.style.backgroundColor="white";
+                                                });
+                                                            green.addEventListener('click', ()=>{
+                                                                paintGreen();
+                                                                let colorMenuActive=document.querySelector('.colorMenuActive');
+                                                                colorMenuActive.style.display="none";
+                                                            });
+                                                                green.addEventListener('mousedown', ()=>{
+                                                                    green.style.backgroundColor="gray";
+                                                                });
+                                                                green.addEventListener('mouseup', ()=>{
+                                                                    green.style.backgroundColor="white";
+                                                                });
 eraser.addEventListener('click', ()=>{
     eraseColor();
 });
@@ -115,16 +154,37 @@ function paintBlack(){
     });
 }
 function paintColor(){
-    document.querySelectorAll('.box').forEach(item => {
-        item.addEventListener('mouseover', ()=>{
-            item.style.background="blue";
-        });
-    });
-}
+        let colorMenu=document.querySelector('.colorMenu');
+        let colorMenuActive=document.querySelector('.colorMenuActive');
+        colorMenu.classList.add('colorMenuActive');
+      
+};
+        function paintRed(){
+            document.querySelectorAll('.box').forEach(item => {
+                item.addEventListener('mouseover', ()=>{
+                    item.style.background="red";
+                });
+            });
+        }
+        function paintBlue(){
+            document.querySelectorAll('.box').forEach(item => {
+                item.addEventListener('mouseover', ()=>{
+                    item.style.background="blue";
+                });
+            });
+        }
+        function paintGreen(){
+            document.querySelectorAll('.box').forEach(item => {
+                item.addEventListener('mouseover', ()=>{
+                    item.style.background="green";
+                });
+            });
+        }
+
 function eraseColor(){
     document.querySelectorAll('.box').forEach(item => {
         item.addEventListener('mouseover', ()=>{
             item.style.background="white";
         });
     });
-}
+};
